22d82264c88ae83fd0adb8040a227d57f70f5190
Maksim Kutishev <kutishev2005@inbox.ru>
2022-08-18 18:56:49 +0600

    added equality operator

--- RationalNumber.py
+++ RationalNumber.py
@@ -1,3 +1,12 @@
+def euclid_algorithm(a, b):
+    c = a % b
+    while c:
+        a = b
+        b = c
+        c = a % b
+    return b
+
+
 class RationalNumber:
     def __init__(self, arg1=0, arg2=1):
         self.arg1 = arg1
@@ -6,6 +15,24 @@ class RationalNumber:
 
     def __repr__(self):
         return f'{self.arg1}/{self.arg2}'
+
+    def __eq__(self, other):
+        gcd1 = euclid_algorithm(self.arg1, self.arg2)
+        gcd2 = euclid_algorithm(other.arg1, other.arg2)
+        numerator1, denominator1 = self.arg1 // gcd1, self.arg2 // gcd1
+        numerator2, denominator2 = other.arg1 // gcd2, other.arg2 // gcd2
+
+        return (numerator1, denominator1) == (numerator2, denominator2)
+
+
+def test_equality_unnormalized():
+    a1 = RationalNumber(1, 2)
+    a2 = RationalNumber(2, 4)
+    assert a1 == a2
+
+
+def test_equality():
+    assert RationalNumber() == RationalNumber(0, 1)
 
 
 def test_rational_repr():
